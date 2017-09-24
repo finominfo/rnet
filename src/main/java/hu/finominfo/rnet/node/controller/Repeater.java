8da89cc41f842a4223dd35f883f30f7b6fbd8ede
Kalman Kovacs <kalman.kovacs@gmail.com>
2017-09-24 21:27:36 +0200

    File sending and receiving

--- src/main/java/hu/finominfo/rnet/node/controller/Repeater.java
+++ src/main/java/hu/finominfo/rnet/node/controller/Repeater.java
@@ -1,9 +1,7 @@
 package hu.finominfo.rnet.node.controller;
 
 import hu.finominfo.rnet.common.Globals;
-import hu.finominfo.rnet.common.Task;
 import hu.finominfo.rnet.common.TaskToDo;
-import hu.finominfo.rnet.communication.tcp.events.Event;
 import org.apache.log4j.Logger;
 
 import java.util.concurrent.TimeUnit;
@@ -19,8 +17,6 @@ public class Repeater implements Runnable {
         try {
             if (Globals.get().isTasksEmpty()) {
                 Globals.get().addToTasksIfNotExists(TaskToDo.SEND_BROADCAST);
-            } else {
-                //logger.info("Number of tasks in queue: " + Globals.get().tasks.size() + " first: " + Globals.get().tasks.peek().getTaskToDo().toString());
             }
         }catch (Exception e) {
             logger.error(e);
