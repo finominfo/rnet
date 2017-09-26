b72f0008a24a0fdeb9c541b552d1668e02b16cc8
finominfo <kalman.kovacs@gmail.com>
2017-09-26 12:30:43 +0200

    Ordering

--- src/main/java/hu/finominfo/rnet/communication/tcp/events/file/FileEventHandler.java
+++ src/main/java/hu/finominfo/rnet/communication/tcp/events/file/FileEventHandler.java
@@ -8,16 +8,52 @@ import org.apache.log4j.Logger;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by kalman.kovacs@gmail.com on 2017.09.21..
  */
-public class FileEventHandler extends SimpleChannelInboundHandler<FileEvent> {
+public class FileEventHandler extends SimpleChannelInboundHandler<FileEvent> implements Runnable{
     private final static Logger logger = Logger.getLogger(FileEventHandler.class);
     private volatile String lastFileName = null;
+    private final Queue<FileEvent> fileEvents = new ArrayDeque<>();
+    private final AtomicBoolean running = new AtomicBoolean(false);
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, FileEvent msg) throws Exception {
+        fileEvents.add(msg);
+        startSaving();
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            while (!fileEvents.isEmpty()) {
+                saveFile(fileEvents.poll());
+            }
+        } catch (Exception e) {
+            logger.error(e);
+        } finally {
+            running.set(false);
+        }
+        if (!fileEvents.isEmpty()) {
+            startSaving();
+        }
+
+    }
+
+    private void startSaving() {
+        if (running.compareAndSet(false, true)) {
+            Globals.get().executor.submit(this);
+        }
+    }
+
+    private void saveFile(FileEvent msg) throws Exception {
         String path = null;
         switch (msg.getFileType()) {
             case MAIN:
@@ -54,4 +90,5 @@ public class FileEventHandler extends SimpleChannelInboundHandler<FileEvent> {
             }
         }
     }
+
 }
